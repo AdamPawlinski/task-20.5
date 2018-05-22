92caa3a21b7c8339cfa0d0a8dc8475e5bd7ca752
Adam Pawliński <adam.pawlinski@gmail.com>
2018-05-22 21:12:27 +0200

    navigation and presentaional components prepared

--- src/index.js
+++ src/index.js
@@ -4,15 +4,14 @@ import { Provider } from 'react-redux';
 import store from './store';
 import DevTools from './DevTools';
 import {getCountries} from './actions/actions';
+import { Router, hashHistory } from 'react-router';
+import routes from './routes';
+
 
 render(
   <Provider store={store}>
-    <div>
-      <h1>Inicjalizacja projektu</h1>
-      <DevTools/>
-    </div>
+    <Router history={hashHistory} routes={routes}>
+    </Router>
   </Provider>,
     document.getElementById('root')
 );
-
-store.dispatch(getCountries());
